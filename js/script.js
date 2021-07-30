13513f9e4b92c9646561d1d3e51f8552bfe5fa6e
Karen Fred <85520953+karen-fred@users.noreply.github.com>
2021-07-30 16:47:44 -0500

    Fetched user data

---
+++ js/script.js
@@ -0,0 +1,29 @@
+//Create global variables
+const about = document.querySelector(".overview");
+const username = "karen-fred";
+
+//Fetch API JSON Data
+const gitUserInfo = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    displayUserInfo(data);
+};
+
+gitUserInfo();
+
+const displayUserInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = `
+        <figure>
+            <img alt="user avatar" src=${data.avatar_url} />
+        </figure>
+        <div>
+            <p><strong>Name:</strong> ${data.name}</p>
+            <p><strong>Bio:</strong> ${data.bio}</p>
+            <p><strong>Location:</strong> ${data.location}</p>
+            <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
+        </div>
+    `;
+    about.append(div);
+};
